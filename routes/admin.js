4c4453545d751fef62418f8a666435a0d7a15b6a
Arghya Jana <arghya0017@gmail.com>
2024-07-09 05:09:20 +0530

    everything done except the cart has some issues

--- routes/admin.js
+++ routes/admin.js
@@ -9,5 +9,8 @@ router.get("/add-product", adminControllers.getAddProduct);
 router.get("/products", adminControllers.getProducts);
 
 router.post("/add-product", adminControllers.postAddProduct);
+router.get("/edit-product/:productId", adminControllers.getEditProduct);
+router.post("/edit-product", adminControllers.postEditProduct);
+router.post("/delete-product",adminControllers.postDeleteProduct)
 
 module.exports = router;
